65fb26cdeb87f21755d26576f76d0d4e131f97cc
Ekaterina Bulyga <59512454+bulygaea@users.noreply.github.com>
2022-04-21 15:32:16 +0300

    Update Task7.py

--- HW1/Task7.py
+++ HW1/Task7.py
@@ -1,11 +1,10 @@
 a = int(input())
 b = int(input())
-day = 1
+day = 0
 
 while a < b:
+    day += 1
     print(f"{day}-й день: {a:3.2f}")
     a *= 1.1
-    day += 1
-print(f"{day}-й день: {a:3.2f}")
 
 print(f"На {day}-й день спортсмен достиг результата - не менее {b} км.")
