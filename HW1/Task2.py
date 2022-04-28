bb897cba264d7e4fd79edc3b0abbdf111deaf554
bulygaea <bulyga.ea@gmail.com>
2022-04-28 18:04:02 +0300

    Update Task2.py

--- HW1/Task2.py
+++ HW1/Task2.py
@@ -2,5 +2,4 @@ input_seconds = int(input())
 seconds = input_seconds % 60
 minutes = input_seconds // 60 % 60
 hours = input_seconds // 3600
-print(f"{f'0{hours}' if hours < 10 else hours}:{f'0{minutes}' if minutes < 10 else minutes}:"
-      f"{f'0{seconds}' if seconds < 10 else seconds}")
+print(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
