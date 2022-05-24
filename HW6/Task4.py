323e7d383dfa43d59346be950cd7ded967b528f8
bulygaea <bulyga.ea@gmail.com>
2022-05-25 01:59:40 +0300

    HW6: Updated Task4.py

--- HW6/Task4.py
+++ HW6/Task4.py
@@ -57,7 +57,7 @@ class WorkCar(Car):
 
 class PoliceCar(Car):
     def __init__(self, speed, color, name):
-        super(PoliceCar, self).__init__(speed, color, name, True)
+        super().__init__(speed, color, name, True)
 
 
 town_car = TownCar(10, 'black', 'Wolkswagen Polo')
