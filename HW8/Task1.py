e169d5a52731b5e0c0d981fbff30ae8d07e6906a
bulygaea <bulyga.ea@gmail.com>
2022-05-28 14:52:52 +0400

    HW8: Updated Task1.py

--- HW8/Task1.py
+++ HW8/Task1.py
@@ -1,15 +1,19 @@
 class Date:
     date = ''
 
-    def __init__(self, date):
+    @staticmethod
+    def __init__(date):
         Date.date = date
 
     @classmethod
     def date_to_int(cls):
         try:
-            return tuple(map(int, Date.date.split('-')))
+            if len(tuple(Date.date.split('-'))) == 3:
+                return tuple(map(int, Date.date.split('-')))
+            else:
+                raise ValueError()
         except Exception as err:
-            return err
+            raise err
 
     @staticmethod
     def is_date_valid():
@@ -17,9 +21,9 @@ class Date:
         try:
             return 1 <= checked_date[0] <= 31 and 1 <= checked_date[1] <= 12 and checked_date[2] > 0
         except Exception as err:
-            return err
+            raise err
 
 
-date = Date(input("Type a date (for ex. 01-01-1970): "))
+Date(input("Type a date (for ex. 01-01-1970): "))
 print(Date.date_to_int())
 print(Date.is_date_valid())
