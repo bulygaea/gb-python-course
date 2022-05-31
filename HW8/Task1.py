581356840194d6c97397e0da8ad6d46aa1a6be6d
bulygaea <bulyga.ea@gmail.com>
2022-05-31 19:16:18 +0400

    HW8: Updated Task1.py

--- HW8/Task1.py
+++ HW8/Task1.py
@@ -1,29 +1,28 @@
 class Date:
-    date = ''
 
-    @staticmethod
-    def __init__(date):
-        Date.date = date
+    def __init__(self, date):
+        self.__date = Date.__date_to_int(date)
 
     @classmethod
-    def date_to_int(cls):
+    def __date_to_int(cls, string_date):
         try:
-            if len(tuple(Date.date.split('-'))) == 3:
-                return tuple(map(int, Date.date.split('-')))
+            result_date = tuple(map(int, string_date.split('-')))
+            if Date.__is_date_valid(result_date):
+                return result_date
             else:
                 raise ValueError()
         except Exception as err:
             raise err
 
     @staticmethod
-    def is_date_valid():
-        checked_date = Date.date_to_int()
-        try:
-            return 1 <= checked_date[0] <= 31 and 1 <= checked_date[1] <= 12 and checked_date[2] > 0
-        except Exception as err:
-            raise err
+    def __is_date_valid(checked_date):
+        if len(checked_date) != 3:
+            return False
+        return 1 <= checked_date[0] <= 31 and 1 <= checked_date[1] <= 12 and checked_date[2] > 0
+
+    def __str__(self):
+        return f'{self.__date[0]:02d}-{self.__date[1]:02d}-{self.__date[2]}'
 
 
-Date(input("Type a date (for ex. 01-01-1970): "))
-print(Date.date_to_int())
-print(Date.is_date_valid())
+new_date = Date(input("Type a date (for ex. 01-01-1970): "))
+print(new_date)
